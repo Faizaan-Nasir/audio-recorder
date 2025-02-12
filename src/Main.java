de2fb6ee7d0b66ab3ec9122e6cfbccab40f80b3d
Aman Kumar Giri <amangiri.dev@gmail.com>
2025-02-12 14:26:08 +0530

    Created New branch , changed variable name and introduced global contants ,New changes incoming.

--- src/Main.java
+++ src/Main.java
@@ -15,20 +15,35 @@ import javafx.stage.DirectoryChooser;
 import java.io.File;
 
 public class Main extends Application {
-
     public static final String FONT_PATH = "/resources/fonts/Jersey10-Regular.ttf";
+    public static final String MICROPHONE_ICON_PATH = "/resources/microphone.png";
+    public static final String STYLES_PATH = "/styles/styles.css";
+    public static final int WINDOW_WIDTH = 900;
+    public static final int WINDOW_HEIGHT = 600;
     private Stage primaryStage;
     private HBox directoryContainer;
+    private String currentDirectory = "c:/users/downloads";
 
-    public void changedirectory() {
+    public void changeDirectory() {
         Button chooseDirButton = new Button("Choose Dir");
         chooseDirButton.setMinWidth(120);
         chooseDirButton.setMaxWidth(120);
         chooseDirButton.setMaxHeight(20);
         chooseDirButton.setId("choosedirbutton");
 
-        Label directoryLabel = new Label("c:user/downloads");
+        Label directoryLabel = new Label(currentDirectory);
         directoryLabel.setId("directorylabel");
+
+        // Add directory chooser functionality
+        chooseDirButton.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+
+            if (selectedDirectory != null) {
+                currentDirectory = selectedDirectory.getAbsolutePath();
+                directoryLabel.setText(currentDirectory);
+            }
+        });
 
         directoryContainer = new HBox(20, chooseDirButton, directoryLabel);
         directoryContainer.setLayoutX(30);
@@ -39,7 +54,7 @@ public class Main extends Application {
         directoryLabel.setMaxWidth(Double.MAX_VALUE);
     }
 
-    public void Dashboard() {
+    public void showDashboard() {
         Pane dashboardRoot = new Pane();
         Font.loadFont(getClass().getResourceAsStream(FONT_PATH), 70);
 
@@ -54,14 +69,14 @@ public class Main extends Application {
         backButton.setMaxWidth(90);
         backButton.setMaxHeight(10);
         backButton.setId("backbutton");
-        backButton.setOnAction(e -> start(primaryStage));
+        backButton.setOnAction(e -> showHome());
 
         HBox dashbuttonContainer = new HBox(backButton);
         dashbuttonContainer.setLayoutX(780);
         dashbuttonContainer.setLayoutY(20);
         dashbuttonContainer.setMinWidth(100);
 
-        changedirectory();
+        changeDirectory();
 
         Button recordingButton = new Button("recording 1");
         recordingButton.setMinWidth(130);
@@ -79,10 +94,10 @@ public class Main extends Application {
         Scene dashboardScene = new Scene(dashboardRoot, 900, 600);
         primaryStage.setTitle("Dashboard");
         primaryStage.setScene(dashboardScene);
-        dashboardScene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+        dashboardScene.getStylesheets().add(getClass().getResource(STYLES_PATH).toExternalForm());
     }
 
-    public void Record() {
+    public void startRecording() {
         Pane recordRoot = new Pane();
         Font.loadFont(getClass().getResourceAsStream(FONT_PATH), 70);
         Image gifImage = new Image(getClass().getResource("/resources/waves.gif").toExternalForm());
@@ -98,20 +113,20 @@ public class Main extends Application {
         backButton.setMaxWidth(90);
         backButton.setMaxHeight(10);
         backButton.setId("backbutton");
-        backButton.setOnAction(e -> start(primaryStage));
+        backButton.setOnAction(e -> showHome());
 
         HBox backbuttonContainer = new HBox(backButton);
         backbuttonContainer.setLayoutX(780);
         backbuttonContainer.setLayoutY(20);
         backbuttonContainer.setMinWidth(100);
 
-        Button stoprecord = new Button("stop rec...");
-        stoprecord.setMinWidth(150);
-        stoprecord.setMaxWidth(150);
-        stoprecord.setMaxHeight(40);
-        stoprecord.setOnAction(e -> Dashboard());
+        Button stopRecordButton = new Button("Stop Recording");
+        stopRecordButton.setMinWidth(150);
+        stopRecordButton.setMaxWidth(150);
+        stopRecordButton.setMaxHeight(40);
+        stopRecordButton.setOnAction(e -> showDashboard());
 
-        HBox stoprecbuttonContainer = new HBox(stoprecord);
+        HBox stoprecbuttonContainer = new HBox(stopRecordButton);
         stoprecbuttonContainer.setLayoutX(350);
         stoprecbuttonContainer.setLayoutY(400);
         stoprecbuttonContainer.setMinWidth(150);
@@ -120,55 +135,65 @@ public class Main extends Application {
         Scene recordScene = new Scene(recordRoot, 900, 600);
         primaryStage.setTitle("recording...");
         primaryStage.setScene(recordScene);
-        recordScene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+        recordScene.getStylesheets().add(getClass().getResource(STYLES_PATH).toExternalForm());
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+    private void showHome() {
+        setupHomeScene();
+    }
+
+    private void setupHomeScene() {
+        Pane homeRoot = new Pane();
         Font.loadFont(getClass().getResourceAsStream(FONT_PATH), 70);
-        Pane homeroot = new Pane();
 
         Label heading = new Label("Voice Recorder");
-        heading.setMinWidth(900);
+        heading.setMinWidth(WINDOW_WIDTH);
         heading.setAlignment(Pos.CENTER);
         heading.setLayoutX(0);
         heading.setLayoutY(200);
         heading.setId("title");
 
-        Image mic_icon = new Image(getClass().getResourceAsStream(FONT_PATH));
-        ImageView micView = new ImageView(mic_icon);
+        Image micIcon = new Image(getClass().getResourceAsStream(MICROPHONE_ICON_PATH));
+        ImageView micView = new ImageView(micIcon);
         micView.setFitWidth(20);
         micView.setFitHeight(25);
 
-        Button record = new Button("record ", micView);
-        record.setContentDisplay(ContentDisplay.RIGHT);
-        record.setAlignment(Pos.CENTER);
-        record.setMinWidth(150);
-        record.setMaxHeight(40);
-        record.setOnAction(e -> Record());
+        Button recordButton = new Button("Record", micView);
+        recordButton.setContentDisplay(ContentDisplay.RIGHT);
+        recordButton.setAlignment(Pos.CENTER);
+        recordButton.setMinWidth(150);
+        recordButton.setMaxHeight(40);
+        recordButton.setOnAction(e -> startRecording());
 
-        Button dashboard = new Button("dashboard");
-        dashboard.setMinWidth(170);
-        dashboard.setMaxWidth(170);
-        dashboard.setMaxHeight(40);
-        dashboard.setOnAction(e -> Dashboard());
+        Button dashboardButton = new Button("Dashboard");
+        dashboardButton.setMinWidth(170);
+        dashboardButton.setMaxWidth(170);
+        dashboardButton.setMaxHeight(40);
+        dashboardButton.setOnAction(e -> showDashboard());
 
-        HBox buttonContainer = new HBox(20, record, dashboard);
+        HBox buttonContainer = new HBox(20, recordButton, dashboardButton);
         buttonContainer.setLayoutY(300);
-        buttonContainer.setMinWidth(900);
+        buttonContainer.setMinWidth(WINDOW_WIDTH);
         buttonContainer.setAlignment(Pos.CENTER);
 
-        homeroot.getChildren().addAll(heading, buttonContainer);
-        Scene scene = new Scene(homeroot, 900, 600);
-        scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+        homeRoot.getChildren().addAll(heading, buttonContainer);
+        Scene scene = new Scene(homeRoot, WINDOW_WIDTH, WINDOW_HEIGHT);
+        scene.getStylesheets().add(getClass().getResource(STYLES_PATH).toExternalForm());
 
         primaryStage.setTitle("Audio Recorder");
         primaryStage.setScene(scene);
-        primaryStage.setMaxWidth(900);
-        primaryStage.setMaxHeight(600);
-        primaryStage.setMinWidth(900);
-        primaryStage.setMinHeight(600);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+        primaryStage.setMaxWidth(WINDOW_WIDTH);
+        primaryStage.setMaxHeight(WINDOW_HEIGHT);
+        primaryStage.setMinWidth(WINDOW_WIDTH);
+        primaryStage.setMinHeight(WINDOW_HEIGHT);
+
+        showHome();
         primaryStage.show();
     }
 
